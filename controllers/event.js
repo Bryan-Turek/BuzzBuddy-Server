feb583103bce9ac970ed5e00c41ff0dbb53c221c
bryan <bryan@bryan-linux.(none)>
2014-04-23 20:30:28 -0400

    Fixed find event by id

--- controllers/event.js
+++ controllers/event.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
         });
     });
     app.get('/events/:id', function (req, res) {
-        Event.findOneById(req.params.id, function(err, event) {
+        Event.findOne({_id: req.params.id}, function(err, event) {
             if(err) {
                console.log(err);
             }
